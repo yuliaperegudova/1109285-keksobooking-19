e4e2a81eb05f927b9d2f510a44a5515d61090516
Yuliaperegudova <yuse4ka08@gmail.com>
2020-02-05 23:57:02 +0300

    Внесла правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var TITLES = ['Уютная квартира', 'Стильные апартаменты', 'Эко бунгало', 'Дом для вечеринок'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECK_TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
 document.querySelector('.map').classList.remove('map--faded');
 
 var getRandomInt = function (min, max) {
@@ -18,57 +26,49 @@ var shuffleArray = function (array) {
   return array;
 };
 
-var PIN_WIDTH = 50;
-var PIN_HEIGHT = 70;
-var TITLES = ['Уютная квартира', 'Стильные апартаменты', 'Эко бунгало', 'Дом для вечеринок'];
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var CHECK_TIMES = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var featuresRandom = [];
-var photosRandom = [];
-
-var getRandomArray = function (newArray, array) {
-  for (var i = 0; i < getRandomInt(1, array.length); i++) {
-    newArray.push(array[i]);
-  }
+var getRandomArray = function (array, newArray) {
+  newArray = array.slice(0, getRandomInt(0, array.length + 1));
   return newArray;
 };
-
-getRandomArray(photosRandom, PHOTOS);
-getRandomArray(featuresRandom, FEATURES);
 
 var similarPins = [];
 
 shuffleArray(CHECK_TIMES);
 shuffleArray(TYPES);
 shuffleArray(TITLES);
+shuffleArray(FEATURES);
+shuffleArray(PHOTOS);
 
 var imgNumber = 1;
-for (var i = 0; i < 8; i++) {
-  var x = getRandomInt(0, 1140 - PIN_WIDTH / 2);
-  var y = getRandomInt(130, 630 - PIN_HEIGHT);
-  similarPins.push(
-      {
-        author: {avatar: 'img/avatars/user0' + imgNumber + '.png'},
-        offer: {
-          title: TITLES[i],
-          address: x + ', ' + y,
-          price: getRandomInt(8000, 100000),
-          type: TYPES[i],
-          rooms: getRandomInt(1, 5),
-          guests: getRandomInt(1, 8),
-          checkin: CHECK_TIMES[i],
-          checkout: CHECK_TIMES[i],
-          features: featuresRandom,
-          description: 'описание', // скорее всего, нужно доработать
-          photos: photosRandom},
-        location: {
-          x: x + PIN_WIDTH / 2, y: y + PIN_HEIGHT}
-      }
-  );
-  imgNumber += 1;
-}
+var createSimilarPinsArray = function () {
+  for (var i = 0; i < 8; i++) {
+    var x = getRandomInt(0, 1140 - PIN_WIDTH / 2);
+    var y = getRandomInt(130, 630 - PIN_HEIGHT);
+    similarPins.push(
+        {
+          author: {avatar: 'img/avatars/user0' + imgNumber + '.png'},
+          offer: {
+            title: TITLES[i],
+            address: x + ', ' + y,
+            price: getRandomInt(8000, 100000),
+            type: TYPES[i],
+            rooms: getRandomInt(1, 5),
+            guests: getRandomInt(1, 8),
+            checkin: CHECK_TIMES[i],
+            checkout: CHECK_TIMES[i],
+            features: getRandomArray(FEATURES),
+            description: 'описание', // скорее всего, нужно доработать
+            photos: getRandomArray(PHOTOS)},
+          location: {
+            x: x + PIN_WIDTH / 2, y: y + PIN_HEIGHT}
+        }
+    );
+    imgNumber += 1;
+  }
+};
+
+createSimilarPinsArray();
+
 
 var mapPinsList = document.querySelector('.map__pins'); // сюда будем вставлять метки
 var pinElementTemplate = document.querySelector('#pin') // шаблон, который будем клонировать
@@ -86,10 +86,9 @@ var renderSimilarPin = function (similarpin) {
 
 var createFragment = function (array) {
   var fragment = document.createDocumentFragment();
-  for (i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     fragment.appendChild(renderSimilarPin(array[i]));
   }
   return fragment;
 };
 mapPinsList.appendChild(createFragment(similarPins));
-
